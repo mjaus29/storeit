ddd4be5e64264fff05de9a4d44deba0090c0bcb3
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-04-28 15:53:05 +0800

    Add new dependencies for UI components and create layout file
    Added "@radix-ui/react-alert-dialog" and "input-otp" dependencies for improved UI functionality. Introduced a root-level layout component for consistent app structure and styling.

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="flex h-screen">
+      Sidebar
+      <section className="flex h-full flex-1 flex-col">
+        <div className="main-content">{children}</div>
+      </section>
+    </main>
+  );
+};
+export default Layout;
