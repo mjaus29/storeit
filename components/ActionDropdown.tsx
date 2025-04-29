a27e15eaa0ec14639e7a053a8a95df8d39c6325f
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-04-29 13:09:19 +0800

    Add file actions: rename, share, delete, and detailed views
    Implemented core file operations: rename, share with users, and delete, along with detailed file views. Updated `ActionDropdown` with modals and integrated associated APIs. Added `ActionsModalContent` to handle file details and sharing functionality.

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -4,7 +4,6 @@ import React, { useState } from "react";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -24,6 +23,13 @@ import Link from "next/link";
 import { constructDownloadUrl } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
+import {
+  deleteFile,
+  renameFile,
+  updateFileUsers,
+} from "@/lib/actions/file.actions";
+import { FileDetails, ShareInput } from "@/components/ActionsModalContent";
 
 const ActionDropdown = ({ file }: { file: Models.Document }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -31,6 +37,9 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
   const [action, setAction] = useState<ActionType | null>(null);
   const [name, setName] = useState(file.name);
   const [isLoading, setIsLoading] = useState(false);
+  const [emails, setEmails] = useState<string[]>([]);
+
+  const path = usePathname();
 
   const closeAllModals = () => {
     setIsModalOpen(false);
@@ -40,7 +49,38 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
     // setEmails([]);
   };
 
-  const handleAction = async () => {};
+  const handleAction = async () => {
+    if (!action) return;
+    setIsLoading(true);
+    let success = false;
+
+    const actions = {
+      rename: () =>
+        renameFile({ fileId: file.$id, name, extension: file.extension, path }),
+      share: () => updateFileUsers({ fileId: file.$id, emails, path }),
+      delete: () =>
+        deleteFile({ fileId: file.$id, bucketFileId: file.bucketFileId, path }),
+    };
+
+    success = await actions[action.value as keyof typeof actions]();
+
+    if (success) closeAllModals();
+
+    setIsLoading(false);
+  };
+
+  const handleRemoveUser = async (email: string) => {
+    const updatedEmails = emails.filter((e) => e !== email);
+
+    const success = await updateFileUsers({
+      fileId: file.$id,
+      emails: updatedEmails,
+      path,
+    });
+
+    if (success) setEmails(updatedEmails);
+    closeAllModals();
+  };
 
   const renderDialogContent = () => {
     if (!action) return null;
@@ -59,6 +99,24 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
               value={name}
               onChange={(e) => setName(e.target.value)}
             />
+          )}
+
+          {value === "details" && <FileDetails file={file} />}
+
+          {value === "share" && (
+            <ShareInput
+              file={file}
+              onInputChange={setEmails}
+              onRemove={handleRemoveUser}
+            />
+          )}
+
+          {value === "delete" && (
+            <p className="delete-confirmation">
+              Are you sure you want to delete {` `}
+              <span className="delete-file-name">{file.name}</span>? This action
+              cannot be undone.
+            </p>
           )}
         </DialogHeader>
 
@@ -95,7 +153,7 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
             alt="dots"
             width={34}
             height={34}
-          />{" "}
+          />
         </DropdownMenuTrigger>
 
         <DropdownMenuContent>
